518511a56183247b6dda0c52018992086b61a0a0
EmmaLawlor <emmalwlr01@gmail.com>
2021-09-16 21:02:07 +0000

    Add docstring to display_hangman function

--- run.py
+++ run.py
@@ -46,10 +46,17 @@ def get_random_word(words):
     return word
 
 def display_hangman(word):
+    """
+    Initialises game by displaying welcome message to user,
+    Displays first stage of hangman image and an
+    underscore for each missing letter of the random word chosen from list
+    """
     stage = 0
     print("Let's play Hangman!\n")
     print(HANGMAN_PICS[stage])
-    print('_' * len(word))
+    print('\n')
+    print('    ','_' * len(word))
+
 
 word = get_random_word(words)
 display_hangman(word)
