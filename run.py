ffc6ec0f70ced78b63e49c13e0354381b12fbcae
EmmaLawlor <emmalwlr01@gmail.com>
2021-09-16 20:57:04 +0000

    Change code for displaying hangman and update readme to credit new source

--- run.py
+++ run.py
@@ -1,6 +1,43 @@
 import random
 from words import words
 
+HANGMAN_PICS = ['''
+     +---+
+         |
+         |
+         |
+        ===''', '''
+     +---+
+     O   |
+         |
+         |
+        ===''', '''
+     +---+
+     O   |
+     |   |
+         |
+        ===''', '''
+     +---+
+     O   |
+    /|   |
+         |
+        ===''', '''
+     +---+
+     O   |
+    /|\  |
+         |
+        ===''', '''
+     +---+
+     O   |
+    /|\  |
+    /    |
+        ===''', '''
+     +---+
+     O   |
+    /|\  |
+    / \  |
+        ===''']
+
 def get_random_word(words):
     """
     Selects random word from word list and returns the word in loswercase
@@ -9,83 +46,10 @@ def get_random_word(words):
     return word
 
 def display_hangman(word):
+    stage = 0
     print("Let's play Hangman!\n")
+    print(HANGMAN_PICS[stage])
     print('_' * len(word))
-
-#Hangman stages taken from https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py
-def display_hangman(tries):
-    stages = [  # final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # initial empty state
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
-    ]
-    return stages[tries]
 
 word = get_random_word(words)
 display_hangman(word)
