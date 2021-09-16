8dc2b7bf78a6ef8ab392bb67c3a27a2db1d9f23a
EmmaLawlor <emmalwlr01@gmail.com>
2021-09-16 21:32:23 +0000

    Add while loop to request letter from user, validate input and check if letter guessed is in the word

--- run.py
+++ run.py
@@ -52,10 +52,23 @@ def display_hangman(word):
     underscore for each missing letter of the random word chosen from list
     """
     stage = 0
+    guessed_letters = []
     print("Let's play Hangman!\n")
     print(HANGMAN_PICS[stage])
     print('\n')
     print('    ','_' * len(word))
+    print('\n')
+    while stage < 7:
+        guess = input('Choose a letter: \n')
+        if guess.lower().strip().isalpha():
+            if guess not in word:
+                print(f'{guess} is not in the word, try again')
+                stage += 1
+                guessed_letters.append(guess)
+            elif guess in guessed_letters:
+                print(f'You already guessed {guess}, try again')
+            else: print(f'{guess} is in the word!')
+        else: print('Invalid input')
 
 
 word = get_random_word(words)
