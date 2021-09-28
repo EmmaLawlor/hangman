5c855a5250e4e2991f1f49ff40ce5a573b75add2
EmmaLawlor <emmalwlr01@gmail.com>
2021-09-28 20:10:06 +0000

    Remove unused, commented-out code

--- run.py
+++ run.py
@@ -45,6 +45,8 @@ class Hangman:
         self.stage = 0
         self.guessed_letters = []
         self.progress = '-' * len(self.word)
+        self.games_played = 0
+        self.games_won = 0
 
     def display_hangman(self):
         print(Fore.CYAN + HANGMAN_PICS[self.stage])
@@ -135,80 +137,6 @@ class Hangman:
         else:
             print('Invalid choice \n')
             self.play_again()
-
-
-#def get_random_word(words):
-#    """
-#    Selects random word from word list and returns the word
-#    """
-#    word = random.choice(words)
-#    return word
-
-
-#def display_hangman(word):
-#    """
-#    Initialises game by displaying welcome message to user,
-#    Displays first stage of hangman image and an
-#    underscore for each missing letter of the random word chosen from list
-#    """
-#    stage = 0
-#    guessed_letters = []
-#    progress = '_' * len(word)
-#    print(Fore.CYAN + HANGMAN_PICS[stage])
-#    print('\n')
-#    print(progress + Style.RESET_ALL)
-#    print('\n')
-
-#    """
-#    While user has lost less than 7 lives,
-#    requests a letter from user & validates input to ensure it is a letter
-#    checks if the letter is in the word and if it has been guessed already.
-#    Gives user appropraite feedback.
-#    Breaks loop when user has correctly guessed all letters
-#   """
-#     while stage < 6:
-#        guess = input(f'{Fore.YELLOW}Choose a letter: {Style.RESET_ALL}')
-#        print('\n')
-#        if guess.lower().strip().isalpha() and len(guess) == 1:
-#            if guess not in word:
-#                if guess in guessed_letters:
-#                    print(f'You already guessed {guess}, try again')
-#                    print('\n')
-#                else:
-#                    print(f'{Fore.RED}{guess} is not in the word, try again{Style.RESET_ALL}')
-#                    print('\n')
-#                   stage += 1
-#                    print(Fore.CYAN + HANGMAN_PICS[stage])
-#                    print('\n')
-#                    print(progress + Style.RESET_ALL)
-#                    print('\n')
-#                    guessed_letters.append(guess)
-#            elif guess in word and guess in guessed_letters:
-#                print(f'You already guessed {guess}, try again')
-#                print('\n')
-#            elif guess in word:
-#                print(f'{Fore.GREEN}{guess} is in the word!{Style.RESET_ALL}')
-#                print('\n')
-#                guessed_letters.append(guess)
-#                # code for replacing underscores with letters adapted from
-#                # https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py
-#                word_as_list = list(progress)
-#                indices = [i for i, letter in enumerate(word) if letter == guess]
-#               for index in indices:
-#                    word_as_list[index] = guess
-#                    progress = "".join(word_as_list)
-#                    print(Fore.CYAN + HANGMAN_PICS[stage])
-#                    print('\n')
-#                    print(progress + Style.RESET_ALL)
-#                    print('\n')
-#                if "_" not in progress:
-#                    print(f'{Fore.GREEN}Congrats! You correctly guessed the answer: {word}{Style.RESET_ALL}')
-#                    print('\n')
-#                    break
-#        else:
-#           print('Invalid input \n')
-#    print(f'{Fore.RED}Game Over! The word was {word}{Style.RESET_ALL}')
-#    print('\n')
 
 
 def main():
